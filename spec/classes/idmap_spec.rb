24ca307daff0818da8cb70291491dd2c5c6de9fc
Mattias Winther <mattias.winther@ericsson.com>
2013-09-27 15:24:32 +0200

    Add misc spec tests
    New spec tests by ebrdann with the current code.

--- spec/classes/idmap_spec.rb
+++ spec/classes/idmap_spec.rb
@@ -70,6 +70,56 @@ describe 'nfs::idmap' do
     }
   end
 
+  context 'with idmap_domain set to valid.tld on EL 5' do
+    let :params do
+      {
+        :idmap_domain   => 'valid.tld',
+      }
+    end
+    let :facts do
+      {
+        :osfamily => 'RedHat',
+        :lsbmajdistrelease => '5',
+      }
+    end
+
+    it {
+      should contain_file('idmapd_conf').with({
+        'ensure' => 'file',
+        'path'   => '/etc/idmapd.conf',
+        'owner'  => 'root',
+        'group'  => 'root',
+        'mode'   => '0644',
+      })
+      should contain_file('idmapd_conf').with_content(/Domain = valid.tld/)
+    }
+  end
+
+  context 'with idmap_domain set to valid.tld on EL 6' do
+    let :params do
+      {
+        :idmap_domain   => 'valid.tld',
+      }
+    end
+    let :facts do
+      {
+        :osfamily => 'RedHat',
+        :lsbmajdistrelease => '6',
+      }
+    end
+
+    it {
+      should contain_file('idmapd_conf').with({
+        'ensure' => 'file',
+        'path'   => '/etc/idmapd.conf',
+        'owner'  => 'root',
+        'group'  => 'root',
+        'mode'   => '0644',
+      })
+      should contain_file('idmapd_conf').with_content(/Domain = valid.tld/)
+    }
+  end
+
   context 'with ldap_base set to a single valid base, dc=edu' do
     let :facts do
       { :osfamily => 'RedHat' }
