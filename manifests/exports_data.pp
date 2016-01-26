98be97e962ebf6661af7828cb7909ec96c6019d7
Vamegh Hedayati <vhedayati@ev9.eu>
2016-01-26 15:29:39 +0000

    pushing up requested changes

--- manifests/exports_data.pp
+++ manifests/exports_data.pp
@@ -1,6 +1,4 @@
 define nfs::exports_data (
-  $config_title = "$title",
-  $message = 'This File is managed by Puppet - NFS Module',
   $path = '',
   $host = '',
   $options = '',
@@ -9,12 +7,10 @@ define nfs::exports_data (
     # * path
     # * host
     # * options
-  concat::fragment { "${config_title}-exports}":
+  concat::fragment { "${title}-exports}":
     target  => "${::nfs::exports::config_name}",
     order   => 10,
-    content => epp("${module_name}/exports_body.epp", {path    => $path,
-                                                       host    => $host,
-                                                       options => $options}),
+    content => template("${module_name}/exports_body.erb"),
   }
 }
 
--- manifests/server.pp
+++ manifests/server.pp
@@ -7,9 +7,28 @@ class nfs::server (
   $exports_owner  = 'root',
   $exports_group  = 'root',
   $exports_mode   = '0644',
+  $exports_data   = '',
+  $ensure         = 'present',
 ) inherits nfs {
-
-  class {'nfs::exports':}
+  $exports_gen = hiera_hash('exports_data',{})
+  if !empty($exports_gen) {
+    concat { "$exports_path":
+      ensure  => "$ensure",
+      path    => "$exports_path",
+      owner   => "$exports_owner",
+      group   => "$exports_group",
+      mode    => "$exports_mode",
+      order   => 'numeric',
+    }
+    # template vars:
+      # * message
+    concat::fragment { "${exports_path}-general}":
+      target  => "$exports_path",
+      order   => 0,
+      content => template("${module_name}/exports_header.erb"),
+    }
+    create_resources('nfs::exports_data', $exports_gen)
+  }
 
   exec { 'update_nfs_exports':
     command     => 'exportfs -ra',
@@ -25,3 +44,4 @@ class nfs::server (
     require    => File['nfs_exports'],
   }
 }
+
